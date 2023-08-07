f020173caca7de8ab08e39284b1dc6bf4128008c
Martin Milan <martin.milan3@gmail.com>
2023-08-07 16:41:42 -0300

    Se agrega exception para key pair

---
+++ auth-server/src/main/java/com/security/accounts/exception/KeyPairException.java
@@ -0,0 +1,11 @@
+package com.security.accounts.exception;
+
+public class KeyPairException extends RuntimeException {
+
+    public KeyPairException() {
+    }
+
+    public KeyPairException(String message) {
+        super(message);
+    }
+}
