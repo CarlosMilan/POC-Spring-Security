910df47df14be3346b02f42bffcca644edfbd7eb
Martin Milan <martin.milan3@gmail.com>
2023-08-08 18:10:20 -0300

    Se agrega proyecto resource server

---
+++ resource-server/src/test/java/com/security/accounts/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
